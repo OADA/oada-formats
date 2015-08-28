29013642feda7fa02043bcc25967977b8c46b088
Andrew Balmos <andrew@balmos.org>
2015-08-28 17:40:04 -0400

    Minor fixups

--- model.js
+++ model.js
@@ -10,15 +10,17 @@ var mediaTypeMap = {
   /////////////////////////////////////////////////////////////////////////////
   // Authorization/Authentication:
   //   - Client Registration
-  'application/vnd.oada.oauth-dyn-reg.register-response.1+json': 'oada/oauth-dyn-reg/register-response.js',
-  
+  'application/vnd.oada.oauth-dyn-reg.register-response.1+json':
+      'oada/oauth-dyn-reg/register-response.js',
+
   /////////////////////////////////////////////////////
   // Bookmarks:
   'application/vnd.oada.bookmarks.1+json': 'oada/bookmarks.js',
 
   /////////////////////////////////////////////////////
   // /.well-known/:
-  'application/vnd.oada.well-known/oada-configuration.1+json': 'oada/well-known/oada-configuration.js',
+  'application/vnd.oada.oada-configuration.1+json':
+      'oada/well-known/oada-configuration.js',
 
 };
 
@@ -27,32 +29,41 @@ var _OADAModel = {
 
   // byShortMediaType: use just the part of the media type that comes after the
   // 'application/vnd.oada.'
-  byShortMediaType: function(short_type, opts) {
+  byShortMediaType: function(shortType, opts) {
     return Promise.try(function() {
-      _OADAModel.byMediaType('application/vnd.oada.' + short_type, opts);
+      _OADAModel.byMediaType('application/vnd.oada.' + shortType, opts);
     });
   },
 
-  byMediaType: function(media_type, opts) {
+  byMediaType: function(mediaType, opts) {
     return Promise.try(function() {
       opts = opts || {};
       // If schema version does not exist, parse it from media type as last number prior to '+' at end:
-      opts.version = opts.version || media_type.replace(/\.([0-9]+)\+/, '$1');
-      var require_str = _.get(mediaTypeMap, media_type, null);
-      if (!require_str) throw new Error('Could not find require string for media type ' + media_type);
-      var handler = require(require_str);
-      if (!handler) throw new Error('Could not require handler for media type ' + media_type);
+      opts.version = opts.version || mediaType.replace(/\.([0-9]+)\+/, '$1');
+
+      var requireStr = _.get(mediaTypeMap, mediaType, null);
+      if (!requireStr)  {
+          throw new Error('Could not find require string for media type ' +
+                  mediaType);
+      }
+
+      var handler = require('./' + requireStr);
+      if (!handler) {
+          throw new Error('Could not require handler for media type ' +
+                  mediaType);
+      }
+
       // Curry the handler to return a model with a particular version
       return handler(opts);
     });
   },
 
   /////////////////////////////////////////////////////////////////////////////
-  // registerMediaType: adds proprietary media type handlers at run-time to 
-  // the OADA-defined ones.  Note you can also override default ones with 
+  // registerMediaType: adds proprietary media type handlers at run-time to
+  // the OADA-defined ones.  Note you can also override default ones with
   // your own.
-  registerMediaType: function(media_type, handler_require_path) {
-    mediaTypeMap[media_type] = handler_require_path;
+  registerMediaType: function(mediaType, handler_require_path) {
+    mediaTypeMap[mediaType] = handler_require_path;
   },
 
   ////////////////////////////////////////////////////////////////////////
--- oada/well-known/oada-configuration.js
+++ oada/well-known/oada-configuration.js
@@ -1,18 +1,23 @@
-var _OADAConfiguration = {
-  example: function() {
-    return { 
-      oada_base_uri: "https://oada.example.com",
-      authorization_endpoint: "https://oada.example.com/authorize",
-      token_endpoint: "https://oada.example.com/token",
-      registration_endpoint: "https://oada.example.com/register",
-      token_endpoint_auth_signing_alg_values_supported: [ "RS256" ],
-    };
-  },
+module.exports = function _OADAConfiguration(version) {
+    return {
+        example: function() {
+            return {
+                well_known_version: "1.0.0",
+                oada_base_uri: "https://oada.example.com",
+                authorization_endpoint: "https://oada.example.com/auth",
+                token_endpoint: "https://oada.example.com/token",
+                registration_endpoint: "https://oada.example.com/register",
+                token_endpoint_auth_signing_alg_values_supported: [ "RS256" ],
+                client_secret_alg_supported: [ "RS256" ]
+                scopes_supported: [{
+                    name: "oada.all.1",
+                    read+write: true
+                }]
+            }
+        },
 
-  validate: function() {
-    throw new Error("validate: not implemented");
-  },
-
+        validate: function() {
+            throw new Error("validate: not implemented");
+        },
+    }
 };
-
-module.exports = new _OADAConfiguration();
