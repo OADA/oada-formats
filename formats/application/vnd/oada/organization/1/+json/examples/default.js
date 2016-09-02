b08b937b0aca036c84904a492f77aac08b9f133d
Aaron Ault <ault@purdue.edu>
2016-09-02 12:58:16 -0400

    Added comments for person and organization

--- formats/application/vnd/oada/organization/1/+json/examples/default.js
+++ formats/application/vnd/oada/organization/1/+json/examples/default.js
@@ -1,10 +1,18 @@
-module.exports = {
-    name: 'Seventh Bank of Omaha',
-    contactPerson: {
-        _id: 'xkadjfixkfjf'
-    },
-    bookmarks: {
-        _id: 'Jkxjdjay3fx',
-        _rev: '34-kjf02i3klj'
-    }
-};
+{
+  _id: "92jfik23o3iwe",
+  _rev: "1-kdufj02ifj2klfklwef",
+  _meta: { _metaid: "92jfik23o3iwe", _rev: "1-kfj029i3jfk2l3" },
+  _type: "application/vnd.oada.organization.1+json",
+
+  // This is an extension of http://schema.org/Organization.  I am focusing on
+  // only data that is of immediate interest for the project at hand, but any
+  // vocabulary from schema.org should be supported:
+
+  name: 'Seventh Bank of Omaha',
+  contactPerson: { _id: 'k2jfko3lf2k3f' }, // schema.org does not have this currently
+
+  // An organization can have it's own bookmarks document.  This would come in handy 
+  // in the case where an organization is a client of another organization, and the
+  // first organization also has clients and other things defined in bookmarks.
+  bookmarks: { _id: 'kj0f2iklj23', _rev: 'kjf02i3klj' },
+}
--- formats/application/vnd/oada/person/1/+json/examples/default.js
+++ formats/application/vnd/oada/person/1/+json/examples/default.js
@@ -1,12 +1,25 @@
-module.exports = {
-    givenName: 'Bob',
-    familyName: 'Smith',
-    email: 'bob@schmob.com',
-    memberOf: {
-        _id: 'ielxu7df8',
-    },
-    bookmarks: {
-        _id: 'akdfj93fad',
-        _rev: '34-kdfjaklsdhx'
-    }
-};
+{
+  _id: "92jfik23o3iwe",
+  _rev: "1-kdufj02ifj2klfklwef",
+  _meta: { _metaid: "92jfik23o3iwe", _rev: "1-kfj029i3jfk2l3" },
+  _type: "application/vnd.oada.person.1+json",
+
+  // This is an extension of http://schema.org/Person.  I am focusing on
+  // only data that is of immediate interest for the project at hand, but any
+  // vocabulary from schema.org should be supported:
+
+  givenName: 'Bob', // in the US, this is the first name
+  familyName: 'Smith', // in the US, this is the last name
+  email: 'bob@schmob.com',
+  memberOf: { _id: 'kdj02lfsd' }, // link to an organization if this person belongs to one
+                                  // note schema.org defines this key, and OADA is adding
+                                  // that it can be a link to an organization
+
+  // A person can have their own bookmarks document, even if they are not a user
+  // with login priviledges.  If, for example, a particular sensor hub is associated
+  // with a particular person, it should reside at that person's bookmarks document.
+  // If that person later becomes a user with login priviledges, that user can 
+  // simply re-use the same bookmarks link.  If they are already a user, then
+  // this link should be the same as that user's bookmarks document.
+  bookmarks: { _id: 'kj0f2iklj23', _rev: 'kjf02i3klj' },
+}
