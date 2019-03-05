5e6ece5d7feaa0f6c24a568f9b9518cdf28f9326
Madushanka Fonseka <madushankaf@325-nb5007.na.sysco.net>
2019-03-05 23:54:05 +0530

    imnplement transformation event message for GS1

---
+++ formats/application/vnd/gs1/produce/transformation_event/1/+json/example/default.js
@@ -0,0 +1,48 @@
+module.exports = {
+
+  _type: 'application/vnd.gs1.produce.transformation_event.1+json',
+  orginator: '1234567890128',
+  trading_partner: '3214567890821',
+  activity_type: 'purchase_order',
+  orginator_type: 'manufacturer',
+  receiver_type: 'distributor',
+  timestamp: '08081028123546',
+  activity_no: 'ABC-123',
+  transformation_input:[
+  {
+    gtin:'97350053850012',
+    batch_or_lot_serial:'234578',
+    product_date:'20180513',
+    sell_by:'1234567890128',
+    quantity:'550',
+    
+  },
+  {
+    gtin:'97350053850012',
+    batch_or_lot_serial:'234578',
+    product_date:'20180513',
+    sell_by:'1234567890128',
+    quantity:'550',
+  },
+
+],
+transformation_output:[
+  {
+    gtin:'78350053850012',
+    batch_or_lot_serial:'234578',
+    product_date:'20180513',
+    sell_by:'1234567890128',
+    quantity:'550',
+    
+  },
+  {
+    gtin:'67350053850012',
+    batch_or_lot_serial:'234578',
+    product_date:'20180513',
+    sell_by:'1234567890128',
+    quantity:'550',
+  },
+
+],
+  
+}
